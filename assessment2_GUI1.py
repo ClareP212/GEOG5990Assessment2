392c2229228fcb2f7773f21684740755edbc8cbd
Clare P <56163311+ClareP212@users.noreply.github.com>
2020-01-20 17:00:52 +0000

    Update assessment2_GUI1.py

--- assessment2_GUI1.py
+++ assessment2_GUI1.py
@@ -39,17 +39,18 @@ for row in ground_zero:
     if sum(row) >0:
        break
 x = ground_zero[y].index(255)
-height = ground_zero[y][x]
+#height = ground_zero[y][x]
 #print(ground_zero[y][x]) 
 #print(height)
 
 def setup():
     """
-    Function which defined the wind direction probabilities based on the GUI,
-    initialises bacteria and feeds in the relevant lists and variables to Bacteria Class
+    Function which defines the wind direction probabilities, hiehgt and number of bacteria based on the GUI,
+    initialises bacteria agents and feeds in the relevant lists and variables to bacteria.
+    Only sets up the bacteria if the wind direvtion probabilities add up to 100.
     Returns:
-        If sliders sum to 100, returns text showing percentages chosen,
-        if less than 100 creates messagebox informing user to ammend sliders
+        If sliders sum to 100, returns text showing percentages chosen, height and number of bacteria.
+        If sliders sum to less than 100 creates messagebox informing the user to ammend sliders.
     """
     global north_perc
     north_perc = (northscale.get()/100) #global variable defining north scale value, updated from GUI slider.
@@ -60,40 +61,42 @@ def setup():
     global west_perc
     west_perc = (westscale.get()/100) #global variable defining west scale value, updated from GUI slider.
     
+    global height
+    height = (heightscale.get()) #global variable defining west scale value, updated from GUI slider.
+    
+    global num_of_bacteria
+    num_of_bacteria = (bacteriascale.get()) #global variable defining west scale value, updated from GUI slider.
 
     if 1 == (north_perc + south_perc + east_perc + west_perc):
         print ("Chance of going South = " + str(west_perc*100) + "%")
         print ("Chance of going East = " + str(east_perc*100) + "%")  
         print ("Chance of going South = " + str(south_perc*100) + "%") 
         print ("Chance of going North = " + str(north_perc*100) + "%")
+        print ("Height = " + str(height))
+        print ("Number of Bacteria = " + str(num_of_bacteria))
         
-        global num_of_bacteria
-        num_of_bacteria = 5000
         global bacteria
         bacteria = []
         for i in range(num_of_bacteria):
-            bacteria.append(Bacteria(ground_zero,height,y,x,north_perc,south_perc,east_perc,west_perc))
+            bacteria.append(Bacteria(height,y,x,north_perc,south_perc,east_perc,west_perc))
     else:
         tkinter.messagebox.showerror("Wind Direction Probabilities","Wind Direction probabilities do not add to 100%, please ammend sliders")
     
 ##Create agents and define their behaviours
 class Bacteria() :
-    def __init__ (self,ground_zero,height,y,x,north_perc,south_perc,east_perc,west_perc):
+    def __init__ (self,height,y,x,north_perc,south_perc,east_perc,west_perc):
         """
         Function to initiate the agent 
         Params:
-            ground_zero - 
-            height - wolf class agent list
-            location - sheep class agent list
-            y - y variable from html in model.
-            x - x variable from html in model.
-            north_perc - 
-            south_perc - 
-            east_perc - 
-            west_perc - 
+            height - the initial height the bacteria are starting at, defined by the user via the height slider in the GUI.
+            y - y variable defining the y-coordinate of the bacterial bomb start location.
+            x - x variable defining the x-coordinate of the bacterial bomb start location.
+            north_perc - the probability of the bacteria moving northwards in any one movement, defined by the user via the north slider in the GUI. 
+            south_perc - the probability of the bacteria moving southwards in any one movement, defined by the user via the south slider in the GUI.
+            east_perc - the probability of the bacteria moving eastwards in any one movement, defined by the user via the east slider in the GUI.
+            west_perc - the probability of the bacteria moving westwards in any one movement, defined by the user via the west slider in the GUI.
             
         """
-        self.ground_zero = ground_zero
         self.y = y
         self.x = x
         self.height = height
@@ -141,15 +144,11 @@ class Bacteria() :
 #        print (self.east_perc)
 #        print (self.west_perc)
 
-####output file####
-#blank output list
-#update output file in module?
 
-            
 def gen_function():
     """
     Function to keep running the model as long as the stopping conditions are not met.
-    Stopping condition is if carry_on = true, carry on is trigegred when agent height reaches 0 (ground).
+    Stopping condition is if carry_on = true, carry on is triggered when bacteria height reaches 0 (ground).
     """
     a = 0
     global carry_on 
@@ -158,30 +157,41 @@ def gen_function():
         a = a + 1   
 
 def update():
+    """
+    Function to run bacteria movement until they hit the ground, record the coordinates where each bacteria hits the ground.
+    Creates an output array of 0's and add 1 to each coordinate for every time a bacteria landed there. Updates the GUI with
+    final map of outputs.
+    
+    Returns:
+        Text denoting the number of bacteria which fell outside of the area plotted on the GUI map and output file.
+    """
     global carry_on
     global bacteria_location
     bacteria_location = []
     
+    #Model bacteria movement until they hit ground (height = 0)
     for i in range(num_of_bacteria):
         carry_on = True
         #bacteria[i].check()
         for j in gen_function(): 
             bacteria[i].move()
             if bacteria[i].height == 0:
+                bacteria_location.append([bacteria[i].y,bacteria[i].x])#attach final x y locations to bacteria_location       
                 carry_on = False
-        bacteria_location.append([bacteria[i].y,bacteria[i].x])#attach final x y locations to bacteria_location    
+        
     
-    #add 1 to each xy in bacteria location list
+    #create output array
     global out_of_range
     out_of_range = 0
     global output
     output = []
     for row in range(500):
-        thing = []
+        col = []
         for i in range (500):
-            thing.append(0)
-        output.append(thing)   # merge w creation
-    
+            col.append(0)
+        output.append(col)   # merge w creation
+        
+    #add 1 to each xy in bacteria location list    
     for i in range(len(bacteria_location)): #remove and merge
         y = bacteria_location[i][0]
         x = bacteria_location[i][1]
@@ -199,6 +209,11 @@ def update():
     print(str(out_of_range) + " Bacteria fell outside of study area.")
 
 def create_output():
+    """
+    Creates a txt file from the populated output array
+    Returns:
+        Prints text to inform user file has been created and where
+    """
     #create txt and populate it with values
     global output
     f = open("output.txt", 'w')
@@ -209,12 +224,11 @@ def create_output():
             f.write(', ')
         f.write('\n')
     f.close
+    print("output.txt created in same file directory as model. :)")
 
 
 ###########GUI###########
-    
-##GUI functions
-    #set figure size
+#set figure size
 fig = matplotlib.pyplot.figure(figsize=(5, 5)) #change to 7,7 on computer
 ax = fig.add_axes([0, 0, 1, 1])
 
@@ -226,7 +240,7 @@ canvas._tkcanvas.pack(side=tkinter.LEFT, fill=tkinter.BOTH, expand=1)
 
 def stop():
     """
-    Function to top the model running and close the model window
+    Function to top the GUI running and close the model window
     """
     root.destroy()
 
@@ -277,65 +291,88 @@ def west_max(value):
     westscale.configure(to=(slider_max(value)))
         
 def default_slider():
+    """
+    Sets slider values to the defaults specified by the assignment brief
+    """
     eastscale.set(75)     
     southscale.set(10)   
     northscale.set(10)   
-    westscale.set(5) 
+    westscale.set(5)
+    heightscale.set(255)
+    bacteriascale.set(5000)
+    
 def reset_slider():
+    """
+    Sets slider values to 0
+    """
     eastscale.set(0)     
     southscale.set(0)   
     northscale.set(0)   
     westscale.set(0)
+    heightscale.set(0)
+    bacteriascale.set(0)
         
 
 ##Create slider bars
-northscale = tkinter.Scale(root, label = "North", from_=0,command=lambda _: north_max(northscale.get()), orient = 'horizontal', length = 200,resolution = 5)
+northscale = tkinter.Scale(root, label = "North", from_=0,command=lambda _: north_max(northscale.get()), orient = 'horizontal', length = 200)
 northscale.pack()
-southscale = tkinter.Scale(root, label = "South", from_=0,command=lambda _: south_max(southscale.get()), orient = 'horizontal', length = 200,resolution = 5)
+southscale = tkinter.Scale(root, label = "South", from_=0,command=lambda _: south_max(southscale.get()), orient = 'horizontal', length = 200)
 southscale.pack()
-eastscale = tkinter.Scale(root, label = "East", from_=0,command=lambda _: east_max(eastscale.get()), orient = 'horizontal', length = 200,resolution = 5)
+eastscale = tkinter.Scale(root, label = "East", from_=0,command=lambda _: east_max(eastscale.get()), orient = 'horizontal', length = 200)
 eastscale.pack()
-westscale = tkinter.Scale(root, label = "West", from_=0,command=lambda _: west_max(westscale.get()), orient = 'horizontal', length = 200,resolution = 5)
+westscale = tkinter.Scale(root, label = "West", from_=0,command=lambda _: west_max(westscale.get()), orient = 'horizontal', length = 200)
 westscale.pack()
 
+heightscale = tkinter.Scale(root, label = "Height", from_=1, to_=1000, orient = 'horizontal', length = 200)
+heightscale.pack()
+
+bacteriascale = tkinter.Scale(root, label = "Number of Bacteria", from_=1, to_=10000, orient = 'horizontal', length = 200)
+bacteriascale.pack()
+
 #GUI buttons
-default_slider = tkinter.Button(root,text="Default slider Values", command=default_slider)
-default_slider.pack(padx=5, pady=20,side='left')
-default_slider = tkinter.Button(root,text="Reset slider Values", command=reset_slider)
-default_slider.pack(padx=5, pady=20,side='left')
+default_butt = tkinter.Button(root,text="Default slider Values", command=default_slider)
+default_butt.pack(padx=5, pady=20,side='left')
+reset_butt = tkinter.Button(root,text="Reset slider Values", command=reset_slider)
+reset_butt.pack(padx=5, pady=20,side='left')
 confirm_setup = tkinter.Button(root,text="Confirm Setup",fg="red", command=setup)
 confirm_setup.pack(padx=5, pady=20,side='left')
 run_butt = tkinter.Button(root,text="RUN",fg="red", command=update)
 run_butt.pack(padx=5, pady=10,side='left')
+output_butt = tkinter.Button(root,text="Create Output .txt", command=create_output)
+output_butt.pack(padx=5, pady=10,side='left')
 run_butt = tkinter.Button(root,text="QUIT",fg="red", command=stop)
 run_butt.pack(padx=5, pady=0,side='left')
 
-#keep animation window running
-tkinter.mainloop()
+
+tkinter.mainloop() # Keep tkinter GUI window running
 
 
 """
-Day 1 - Read in the data, find the cooridnates of bombing point, create random movements NESW and UpDown
-Day 2 - create generator function to run until hits 0, create new module and move in movement functions,
-    get this working with changeable num_of_bacteria var, scrap generator function
-Day 3 - Set up generator function to run iterations for each agent until height 0,
+__Development Log__
+Day 1 - BASICS - Read in the data, find the cooridnates of bombing point, create random movements NESW and UpDown
+Day 2 - BASIC LOOPING - create generator function to run until hits 0, create new module and move in movement functions,
+    get this working with changeable num_of_bacteria var
+Day 3 - Set up generator function to run iterations for each bacteria agent until height 0,
     wrote something to add one to output in location specified by bacteria_location
 Day 4 - got generator function working, produced text file of output
-Day 5 - added anmation, update every bacteria ground but very computationally expensive and takes too long so removed
-Day 6 - fixng the sliders to be max 100 for all 4, usability?? this is very tricky, attempting to fix
-Day 7 - attempting to fix sliders again, function to set maximum value, YAY its working!
-    added messagebox if sliders dont add to 100, integrated module so all in one script, issues with output file if
-    x and y of bacteria is out of range
+Day 5 - added animation, update every bacteria contact with ground but very computationally expensive/takes too long so removed
+Day 6 - fixing the sliders to be max 100 for all 4, usability?? this is tricky, attempting to fix
+Day 7 - attempting to fix sliders again, trying a function to set maximum value instead of auto-adjust, YAY its working!
+    added messagebox if sliders dont add to 100, integrated module so all in one script, tidied things up a bit and added
+    some documentation/commenting, added height and bacteria number sliders and integrated to setup
 
 To do:
-Allowing the user to set
-    the number of particles
-    move with max
-    particle height
-sum to check the total of outputs is equal to the total bacteria input
-make sliders functions more efficient
+refresh button
 change colour of output
 do i need to feed in ground zero to bacteria class?
-add in timer to update?
+add in timer to update? - time the time taken to process un the update function, print "time taken to model... X secs"
+
+resolution 5?
+
+
+change gui so we can see the axis
 set axis to min max x and y
+print x and y ranges, max number of bacteria in one place
+
+user decide file name
 """
